7c74af64e333125797a4d6a1277da0afe69a279c
raasikamurugesan <727821tuec141@skct.edu.in>
2023-04-20 12:47:43 +0530

    Day1-23

---
+++ Day1-23/src/main/java/com/example/demo/Day123Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day123Application.class, args);
+	}
+
+}
